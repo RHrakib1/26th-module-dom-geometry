24288200d51cf9adbc118a5c6125accdcb42cec2
RHrakib1 <rakibhasanmd457@gmail.com>
2024-02-16 19:18:58 +0600

    add to the geometry

--- java.js
+++ java.js
@@ -18,6 +18,8 @@ function calculationBtn() {
     // add
     const newText = document.getElementById('newTextAdd');
     newText.innerText = area;
+    const areaResult = document.getElementById('reult');
+    areaResult.innerText = area;
 }
 
 
@@ -27,23 +29,48 @@ function recangleBtn() {
     const inputFirst1 = document.getElementById('rectangleFirstForm');
     const storage2 = inputFirst1.value;
     const weight = parseFloat(storage2);
-    console.log(weight);
+    // console.log(weight);
     // recangle second 
     const inputSecond1 = document.getElementById('rectangleSecondForm');
     const storageSecond2 = inputSecond1.value;
     const lenght = parseFloat(storageSecond2);
-    console.log(lenght);
+    // console.log(lenght);
 
     // area
-    const recangleArea = weight*lenght;
+    const recangleArea = weight * lenght;
     console.log(recangleArea);
 
-    // add
-    const newText2 = document.getElementById('newTextAdd2');
-    newText2.innerText = recangleArea;
+
+    // add 
+    const add = document.getElementById('newTextAdd1');
+    add.innerText = recangleArea;
 
 
+    // main add 
+    const mainArea = document.getElementById('lastAdd');
+    mainArea.innerText = recangleArea;
 
+
+}
+
+
+// rhombus
+function rhombusBtn() {
+    // first form
+    const getRhombusInput = document.getElementById('rhombusFirst');
+    const importRhombus = getRhombusInput.value;
+    const rhombusFlot = parseFloat(importRhombus);
+    console.log(rhombusFlot);
+
+    // second form
+    const getRhombusInput2 = document.getElementById('rhombusSecond');
+    const importRhombus2 = getRhombusInput2.value;
+    const rhombusFlot2 = parseFloat(importRhombus2);
+    console.log(rhombusFlot2);
+
+    // formula for the rhombus
+    const rhombusArea = 0.5 * rhombusFlot * rhombusFlot2;
+    console.log(rhombusArea);
 }
 
 
